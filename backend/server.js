3538f369635a19a674233697c6b37477cb8fbac9
anand raj <git add README.md>
2026-07-04 09:16:16 +0530

    intialsie db

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import app from "./src/app.js";
+import { connectdb } from './src/config/db.js';
+
+
+
+
+
+
+
+
+
+
+
+connectdb()
+
+
+
+app.listen(3000,()=>{
+    console.log("server is running")
+})
